fc4f2be5df24da687aa4478fe578121b42383103
Alto b <altob282@gmail.com>
2024-02-25 02:01:08 +0530

    carousal added

--- lib/presentation/screens/homepage/homepage.dart
+++ lib/presentation/screens/homepage/homepage.dart
@@ -1,6 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:code_geeks/domain/login_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,6 +24,47 @@ class _HomePageState extends State<HomePage> {
             Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => EntryPage(),), (route) => false);
           }, icon: Icon(Icons.logout))
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,width: 500,
+              color: Colors.blue,
+              child: Icon(Icons.abc,size: 50,),
+            ),
+            CarouselSlider(
+                options: CarouselOptions(
+                  height: 200, 
+                  enlargeCenterPage: true, 
+                  aspectRatio: 16/9, 
+                  //aspectRatio: 4/3,
+                  enableInfiniteScroll: true,
+                  autoPlay: true, 
+                  autoPlayInterval: Duration(seconds: 3), 
+                  autoPlayAnimationDuration: Duration(milliseconds: 1000), 
+                  autoPlayCurve: Curves.fastOutSlowIn, 
+                  //autoPlayCurve: Curves.easeIn,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height, 
+                ),
+                items: [
+                  // Add your carousel items here, e.g., Container, Image, or any widget
+                  Container(
+                    //color: Colors.red,
+                    child: Center(child: Image.asset('lib/assets/logo.png')),
+                  ),
+                  Container(
+                    //color: Colors.blue,
+                    child: Center(child: Image.asset('lib/assets/logo.png')),
+                  ),
+                  Container(
+                    //color: Colors.green,
+                    child: Center(child: Image.asset('lib/assets/logo.png')),
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
 
       // floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
