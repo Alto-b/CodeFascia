a947aa92f6237979d5dffe4471f4d960ddbfe902
Alto b <altob282@gmail.com>
2024-05-10 20:14:24 +0530

    updated after TOI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:CodeFascia/options.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:CodeFascia/application/bnb_bloc/bnb_bloc.dart';
 import 'package:CodeFascia/application/feedback_bloc/feedback_bloc.dart';
@@ -26,95 +27,96 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 // bool internetResult=false;
-Future<void> main() async { 
-
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   bool internetResult = await InternetConnectionChecker().hasConnection;
-    // if(internetResult == true) {
-    //   print('YAY! Free cute dog pics!');
-    // } else {
-    //   print('No internet :( Reason:');
-    // }
+  // if(internetResult == true) {
+  //   print('YAY! Free cute dog pics!');
+  // } else {
+  //   print('No internet :( Reason:');
+  // }
 
-  Platform.isAndroid? await Firebase.initializeApp( 
-      options: const FirebaseOptions( 
-          apiKey: "AIzaSyDFLURsPEzl6KV3Aau3POzWV3JUJd9plz0", 
-          projectId: "code-geeks-ff98c", 
-          messagingSenderId: "688360665265", 
-          appId: "1:688360665265:android:a76df499c29b711251f290",
-          storageBucket: 'code-geeks-ff98c.appspot.com', 
-        ) 
-  ):
- await Firebase.initializeApp();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          // options: const FirebaseOptions(
+          //     apiKey: "AIzaSyDFLURsPEzl6KV3Aau3POzWV3JUJd9plz0",
+          //     projectId: "code-geeks-ff98c",
+          //     messagingSenderId: "688360665265",
+          //     appId: "1:688360665265:android:a76df499c29b711251f290",
+          //     storageBucket: 'code-geeks-ff98c.appspot.com',
+          //   )
+          options: FirebaseConfig.options)
+      : await Firebase.initializeApp();
 
 //  await FirebaseApi().initNotifications();
 
 //  final fcmToken = await FirebaseMessaging.instance.getToken();
 //   await FirebaseMessaging.instance.setAutoInitEnabled(true);
 //   print("FCMToken $fcmToken");
-  
-  runApp( MyApp(internetResult: internetResult,));
-}
 
+  runApp(MyApp(
+    internetResult: internetResult,
+  ));
+}
 
 class MyApp extends StatelessWidget {
   final bool internetResult;
-  const MyApp({super.key,required this.internetResult});
+  const MyApp({super.key, required this.internetResult});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => BnbBloc(),
-
-      child: AdaptiveTheme(
-      light: ThemeData.light(useMaterial3: true),
-      dark: ThemeData.dark(useMaterial3: true),
-      initial: AdaptiveThemeMode.light,
-      builder: (theme, darkTheme) => 
-      MultiBlocProvider(
-          providers: [
-            BlocProvider(
-              create: (context) => HomeBloc(LanguageRepo(),MentorRepo()),
+        create: (context) => BnbBloc(),
+        child: AdaptiveTheme(
+          light: ThemeData.light(useMaterial3: true),
+          dark: ThemeData.dark(useMaterial3: true),
+          initial: AdaptiveThemeMode.light,
+          builder: (theme, darkTheme) => MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (context) => HomeBloc(LanguageRepo(), MentorRepo()),
+              ),
+              BlocProvider(
+                create: (context) => FeedbackBloc(),
+              ),
+              BlocProvider(
+                create: (context) => ImageBloc(),
+              ),
+              BlocProvider(
+                create: (context) => ImagePickerBloc(ImagePickerUtils()),
+              ),
+              BlocProvider(
+                create: (context) => GeminiBloc(),
+              ),
+              BlocProvider(
+                create: (context) => UserBloc(UserRepo()),
+              ),
+              BlocProvider(
+                create: (context) => JoinMentorBloc(),
+              ),
+              BlocProvider(
+                create: (context) =>
+                    SubscriptionBloc(SubscriptionRepo(), UserRepo()),
+              ),
+              BlocProvider(
+                create: (context) => PostBloc(PostRepo()),
+              ),
+              BlocProvider(
+                create: (context) => HistoryBloc(SubscriptionRepo()),
+              ),
+            ],
+            child: MaterialApp(
+              title: 'Code Geeks',
+              debugShowCheckedModeBanner: false,
+              theme: theme,
+              darkTheme: darkTheme,
+              home: internetResult
+                  ? const LoginCheckPage()
+                  : const NoInternetPage(),
             ),
-            BlocProvider(
-              create: (context) => FeedbackBloc(),
-            ),
-              BlocProvider(
-              create: (context) => ImageBloc(),
-      
-            ),
-              BlocProvider(
-                  create: (context) => ImagePickerBloc(ImagePickerUtils()),
-              ),
-               BlocProvider(
-                  create: (context) => GeminiBloc(),
-              ),
-              BlocProvider(
-                  create: (context) => UserBloc(UserRepo()),
-              ),
-              BlocProvider(
-                  create: (context) => JoinMentorBloc(),
-              ),
-              BlocProvider(
-                  create: (context) => SubscriptionBloc(SubscriptionRepo(),UserRepo()),
-              ),
-              BlocProvider(
-                  create: (context) => PostBloc(PostRepo()),
-              ),
-              BlocProvider(
-                  create: (context) => HistoryBloc(SubscriptionRepo()),
-              ),
-          ],
-                  child: MaterialApp(
-                    title: 'Code Geeks',
-                    debugShowCheckedModeBanner: false,
-                    theme: theme,
-                    darkTheme: darkTheme,
-                    home: internetResult ? const LoginCheckPage() : const NoInternetPage(),
-                  ),
-      ),)
-    );
+          ),
+        ));
   }
 }
